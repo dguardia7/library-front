69b11da388a215eef34c007bb557ed44a664a812
dguardiacuervo <dguardia@unal.edu.co>
2023-06-05 20:04:17 -0500

    feat: peticion post FormView

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -5,18 +5,23 @@
             <input type="text" id="nombre" name="nombre" v-model="libro.nombre">
         </div>
         <div>
-            <label for="precio" style="margin-right: 10px">Precio:</label>
-            <input type="number" id="precio" name="precio" v-model="libro.precio" readonly>
+            <label for="autor" style="margin-right: 10px">Autor:</label>
+            <input type="text" id="autor" name="autor" v-model="libro.autor">
         </div>
         <div>
-            <label for="cantidad" style="margin-right: 10px">Cantidad:</label>
-            <input type="number" id="cantidad" name="cantidad" v-model="libro.cantidad">
+            <label for="isbn" style="margin-right: 10px">isbn:</label>
+            <input type="text" id="isbn" name="isbn" v-model="libro.isbn">
+        </div>
+        <div>
+            <label for="lenguaje" style="margin-right: 10px">lenguaje:</label>
+            <input type="text" id="lenguaje" name="lenguaje" v-model="libro.lenguaje">
+        </div>
+        <div>
+            <label for="pages" style="margin-right: 10px">Paginas:</label>
+            <input type="number" id="pages" name="pages" v-model="libro.pages">
         </div>
         <p>
             {{ libro.nombre }}
-        </p>
-        <p>
-            Total: ${{ total }}
         </p>
         <button type="button" @click="create">Crear</button>
     </form>
@@ -24,25 +29,35 @@
 
 <script lang="ts">
 
+import axios from 'axios';
+
 export default {
     name: "FormView",
     data() {
         return {
             libro: {
-                cantidad: 0,
+                isbn: null,
                 nombre: null,
-                precio: 0,
+                autor: null,
+                lenguaje: null,
+                pages: null
             }
         }
     },
-    computed: {
-        total() {
-            return this.libro.cantidad * this.libro.precio
-        }
-    },
+    // computed: {
+    //     total() {
+    //         return this.libro.cantidad * this.libro.precio
+    //     }
+    // },
     methods: {
         create() {
-            alert("El libro: " + this.libro.nombre + " ha sido creado")
+            axios.post('/api/books', this.libro)
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         }
     },
     mounted() {
@@ -51,12 +66,12 @@ export default {
     unmounted() {
         console.log("desmontado")
     },
-    watch: {
-        'libro.nombre': function (newVal, oldVal) {
-            console.log(oldVal, newVal)
-            this.libro.precio = Math.floor(Math.random() * 100000);
-        },
-    }
+    // watch: {
+    //     'libro.nombre': function (newVal, oldVal) {
+    //         console.log(oldVal, newVal)
+    //         this.libro.precio = Math.floor(Math.random() * 100000);
+    //     },
+    // }
 }
 </script>
 
